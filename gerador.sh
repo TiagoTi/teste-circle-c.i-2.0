36608c0ef80a2e963d6b4bfc3fc60188a0d42600
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2018-07-19 07:03:14 -0300

    gerador

---
+++ gerador.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Este script gera commits e branches
+# A intenção é treinar com o circle c.i lhida com visões baseada em com tag
+
+VERSAO=$(cat ./versao)
+
+
+echo $VERSAO
+
+VERSAO=$(( $VERSAO + 1 ))
+RELEASE=0
+TAG=0
+
+
+if [ $(( $VERSAO % 2 )) = 0 ]
+then
+	echo "TAG"
+	TAG=$(( $VERSAO / 2 ))
+	git add .
+	git commit -m "versao: 0.0.$VERSAO"
+	git push
+	git tag -a "v0.$TAG.$VERSAO"  -m "tag-version: 0.$TAG.$VERSAO"
+	git push --tags
+else
+	echo "RELEASE"
+	RELEASE="release-0.$RELEASE.$VERSAO"
+	git checkout -b $RELEASE
+	git add .
+	git commit -m "versao: 0.0.$VERSAO"
+	git push --set-upstream origin $RELEASE
+	git checkout master
+	git branch -D $RELEASE
+fi
+
+echo $VERSAO
+echo $VERSAO > ./versao 
